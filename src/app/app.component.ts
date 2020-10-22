b7c4b620d4779dbc177a72dcfafcdb2d7ec08881
crepin fadjo <fadcrepin@gmail.com>
2020-10-22 21:29:14 +0200

    challenge done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,49 +12,11 @@ export class AppComponent {
   timesMoreFun = 2 + 2
 
   gameConsoles: GameConsole[] = this.gameConsoleService.list()
-  mostSoldGameConsole: GameConsole  = this.getMaxY() 
-  lessSoldGameConsole: GameConsole = this.getMinY()
+  mostSoldGameConsole: GameConsole  = this.gameConsoles.reduce((mostSold, gameConsole) => gameConsole.sales2017 + gameConsole.sales2018 > mostSold.sales2017 + mostSold.sales2018 ? gameConsole: mostSold, this.gameConsoles[0]);
+  lessSoldGameConsole: GameConsole = this.gameConsoles.reduce((lessSold, gameConsole) => gameConsole.sales2017 + gameConsole.sales2018 < lessSold.sales2017 + lessSold.sales2018 ? gameConsole : lessSold, this.gameConsoles[0]);
 
-  
 
   constructor(private gameConsoleService: GameConsoleService) {}
 
-
-  mostSell(){
-    let v:number = 0
-    let t:GameConsole
-    for(let game of this.gameConsoles){
-      let y:number = game.sales2017 + game.sales2018
-
-      if(y>v){
-        v=y
-        t = game
-      }
-    }
-    return t
-  }
-
-
-  lessSell(){
-    let v:number = this.gameConsoles[0].sales2017 + this.gameConsoles[0].sales2018
-    let t:GameConsole
-    for(let game of this.gameConsoles){
-      let y:number = game.sales2017 + game.sales2018
-
-      if(y<v){
-        v=y
-        t = game
-      }
-    }
-    return t
-  }
-
-   getMinY() {
-    return this.gameConsoles.reduce((lessSold, gameConsole) => gameConsole.sales2017 + gameConsole.sales2018 < lessSold.sales2017 + lessSold.sales2018 ? gameConsole : lessSold, this.gameConsoles[0]);
-  }
-   getMaxY() {
-    return this.gameConsoles.reduce((mostSold, gameConsole) => gameConsole.sales2017 + gameConsole.sales2018 > mostSold.sales2017 + mostSold.sales2018 ? gameConsole: mostSold, this.gameConsoles[0]);
-  }
-  
   
 }
